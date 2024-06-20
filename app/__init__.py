d3872ca7605497206b454fdbbbddc200f6043409
Drew Fout <drewmf1566@gmail.com>
2024-06-20 13:17:58 -0500

    root_endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, Blueprint, redirect
 from .config import Config
 from .shipping_form import ShippingForm
 from flask_migrate import Migrate
-from .models import db
+from app.models import db, Package
 
 
 app = Flask(__name__)
@@ -19,9 +19,25 @@ def shipping_request():
     form = ShippingForm()
 
     if form.validate_on_submit():
+
+        data = form.data
+        new_package = Package(sender=data["sender_name"],
+                              recipient=data["recipient_name"],
+                              origin=data["origin"],
+                              destination=data["destination"],
+                              location=data["origin"])
+        db.session.add(new_package)
+        db.session.commit()
         return redirect("/")
 
     return render_template('shipping_request.html', form=form)
+
+
+@bp.route("/")
+def root_endpoint():
+    packages = Package.query.all()
+    print(packages)
+    return render_template('package_status.html', packages=packages)
 
 
 app.register_blueprint(bp)

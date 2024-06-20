f3c889c995d3fb83e70e52b3a4c062fcaf4c1d82
Timothy Macfarlane <tim.3imt@gmail.com>
2024-06-20 11:48:27 -0500

    Create a form to collect data to ship a package

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
 from .config import Config
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+
+bp_shipping_request = Blueprint('bp_shipping_request', __name__)
+
+
+@bp_shipping_request.route('/new_package', methods=['GET', 'LIST'])
+def bp_shipping_request():
+    return render_template('shipping_request.html')

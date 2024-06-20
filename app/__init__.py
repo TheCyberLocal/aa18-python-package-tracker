1d4f2c582b618870531ea01c253fb82821d50659
Timothy Macfarlane <tim.3imt@gmail.com>
2024-06-20 12:41:49 -0500

    Ready for phase 2

--- app/__init__.py
+++ app/__init__.py
@@ -3,20 +3,21 @@ from .config import Config
 from .shipping_form import ShippingForm
 
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 
 
-bp_shipping_request = Blueprint('bp_shipping_request', __name__)
+bp = Blueprint('bp', __name__)
 
 
-@bp_shipping_request.route('/new_package', methods=['GET', 'LIST'])
-def bp_shipping_request():
+@bp.route('/new_package', methods=['GET', 'POST'])
+def shipping_request():
     form = ShippingForm()
+
     if form.validate_on_submit():
-
-
         return redirect("/")
 
-    return render_template('shipping_request.html')
+    return render_template('shipping_request.html', form=form)
+
+
+app.register_blueprint(bp)
--- app/config.py
+++ app/config.py
@@ -2,3 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
